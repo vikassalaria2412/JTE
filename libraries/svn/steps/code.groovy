0a3cb749d9599916af2e6b07b377429169b47ff4
Vikas Salaria <vsalaria@dminc.com>
2023-06-22 19:29:08 +0530

    changed jenkinsfile directory location5

--- libraries/svn/steps/code.groovy
+++ libraries/svn/steps/code.groovy
@@ -2,36 +2,39 @@
 void call()
 {
     stage('svn:code') {
+        agent {
+            label 'jenkins-slave' // Replace with the label of your Jenkins slave
+    }
            
                 // Checkout code from SVN repository changes
-                checkout([$class: 'SubversionSCM', 
-                additionalCredentials: [], 
-                excludedCommitMessages: '', 
-                excludedRegions: '', 
-                excludedRevprop: '', 
-                excludedUsers: '', 
-                filterChangelog: false, 
-                ignoreDirPropChanges: false, 
-                includedRegions: '', 
-                locations: [[credentialsId: 'svn_credential_pipeline', 
-                           depthOption: 'infinity', 
-                           ignoreExternalsOption: true, 
-                           remote: 'https://svn.riouxsvn.com/dmifactory']], 
-                workspaceUpdater: [$class: 'UpdateUpdater']])
-                script {
-                    def currentDir = sh(returnStdout: true,script: 'pwd').trim()
-                // Print current directory
-                    echo "Current directory: ${currentDir}"
-                // List contents of the current directory
-                    sh "ls ${currentDir}"
-                //var = echo 'Current directory: ' + pwd()
-                    sh 'ls /home/jenkins/agent/workspace/vikas_test_pipeline_new/dmifactory'
-                    
-                }
-                // Set the path to the build.xml file
-                
+        checkout([$class: 'SubversionSCM', 
+        additionalCredentials: [], 
+        excludedCommitMessages: '', 
+        excludedRegions: '', 
+        excludedRevprop: '', 
+        excludedUsers: '', 
+        filterChangelog: false, 
+        ignoreDirPropChanges: false, 
+        includedRegions: '', 
+        locations: [[credentialsId: 'svn_credential_pipeline', 
+                    depthOption: 'infinity', 
+                    ignoreExternalsOption: true, 
+                    remote: 'https://svn.riouxsvn.com/dmifactory']], 
+        workspaceUpdater: [$class: 'UpdateUpdater']])
+        script {
+            def currentDir = sh(returnStdout: true,script: 'pwd').trim()
+        // Print current directory
+            echo "Current directory: ${currentDir}"
+        // List contents of the current directory
+            sh "ls ${currentDir}"
+        //var = echo 'Current directory: ' + pwd()
+            sh 'ls /home/jenkins/agent/workspace/vikas_test_pipeline_new/dmifactory'
             
-
         }
+        // Set the path to the build.xml file
+        
     
+
+}
+
 }
