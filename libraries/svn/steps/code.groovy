b654cabc1323fe8627ef1fd5b5f45aa35a5c108a
Vikas Salaria <vsalaria@dminc.com>
2023-06-22 19:51:43 +0530

    changed jenkinsfile directory location14

--- libraries/svn/steps/code.groovy
+++ libraries/svn/steps/code.groovy
@@ -22,16 +22,7 @@ void call()
                     ignoreExternalsOption: true, 
                     remote: 'https://svn.riouxsvn.com/dmifactory']],
         workspaceUpdater: [$class: 'UpdateUpdater']])
-        script {
-            def currentDir = sh(returnStdout: true,script: 'pwd').trim()
-        // Print current directory
-            echo "Current directory: ${currentDir}"
-        // List contents of the current directory
-            sh "ls ${currentDir}"
-        //var = echo 'Current directory: ' + pwd()
-            sh 'ls /home/jenkins/agent/workspace/vikas_test_pipeline_new/dmifactory'
-            
-        }
+        sh 'ls -l'
         // Set the path to the build.xml file
         
     
